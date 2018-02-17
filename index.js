a462b0b633115bfc2953b42d63b5231065c52176
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-02-17 16:56:20 +0900

    Add AlbumList.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-  <Header headerText={'Albumsss'} />
+  <View>
+    <Header headerText={'Albumsss'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
